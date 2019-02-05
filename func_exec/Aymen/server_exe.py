5a67c5a76f5015071b4c3fe49642120af82610a2
Aymen <aymenfadhel@yahoo.com>
2019-02-05 11:49:40 -0500

    import module with exec

--- func_exec/Aymen/server_exe.py
+++ func_exec/Aymen/server_exe.py
@@ -1,19 +1,15 @@
 #!/bin/python
 import sys
 import os
-import time
 import argparse
-import ConfigParser
-from subprocess import Popen, PIPE
 import  socket
-import subprocess
-from math import *
 import json
 import pickle
-
+import ast 
+import importlib 
 
 class Dirty_server():
-
+   
 
     def server(self):
 
@@ -25,27 +21,42 @@ class Dirty_server():
         s.listen(1)
         print("Now listening...\n")
         self.conn, self.addr = s.accept()
+	print 'Starting New connection from %s:%d' % (self.addr[0],self.addr[1])
+
+    def recv(self):
+
+        self.rec_data = self.conn.recv(1024)
+	print 'Received data  :',self.rec_data
+
+    def import_module(self,name):
+
+    	mod = __import__(name)
+    	for s in name.split('.')[1:]:
+        	mod = getattr(mod, s)
+    	print name,'module loaded\n'
+	return mod
+	
 
     def exe(self):
-
-
-        print 'Starting New connection from %s:%d' % (self.addr[0],self.addr[1])
-        data = self.conn.recv(1024)
-        print 'Data received..  ', data
-        cmd = data
-        #stdout=execfile(cmd)
-        stdout=eval(compile(cmd, '<string>', 'eval'))
-
-        serial = json.dumps(stdout)
-        print 'Sending the result back ....\n'
+	
+    	 
+	data=json.loads(self.rec_data)
+	mod=data[0]
+	exec "import {}".format(mod)
+	print 'Data received & executing\n'
+	cmd = (data[1])
+        std = eval(compile(cmd, '<string>', 'eval'))
+        serial = json.dumps(std)
+        print ('Sending the result back ...')
         self.conn.send(serial)
         self.conn.close()
-        print 'closing connection...\n'
+        print ('closing connection...')
 
 
 
 ss=Dirty_server()
 ss.server()
+ss.recv()
 ss.exe()
 
 
