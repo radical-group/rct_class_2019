7a95200019b0f5d29b557ab96049022d9b16140d
Jumana Dakka <jumanadakka@gmail.com>
2019-02-26 15:10:29 -0500

    created folder and started setup for scheduler

---
+++ projects/group2/scheduler.py
@@ -0,0 +1,24 @@
+# Implements naive FIFO scheduler (thread safe)
+
+import Queue
+
+class fifo_scheduler(object):
+
+    def __init__(self, priority, description):
+        self.priority = priority
+        self.description = description
+        print 'New job:', description
+        return
+
+    def queue_fifo(self, units):
+
+        # dummy cus to mimic cu description
+        self.cus = OrderedDict((x,x) for x in reversed(range(10)) )
+
+        q = Queue.Queue()
+
+        for i in self.cus:
+            q.put(i)
+
+        while not q.empty():
+            print q.get()
